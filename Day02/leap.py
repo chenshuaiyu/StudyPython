8b2a19b0345727625b7be80496000dac1a2d1ffc
chenshuaiyu <664251867@qq.com>
2019-04-25 10:39:09 +0800

    Day02

---
+++ Day02/leap.py
@@ -0,0 +1,4 @@
+# 判断是否为闰年
+year = int(input('请出入年份：'))
+is_leap = ((year % 4 == 0 and year % 100 != 0) or (year % 400 ==0))
+print(is_leap)
---
+++ Day02/string.py
@@ -0,0 +1,12 @@
+str1 = 'hello world!'
+print('字符串的长度是：', len(str1))
+print('单词首字母大写：', str1.title())
+print('字符串变大写：', str1.upper())
+# str1 = str1.upper()
+print('字符串是不是大写：', str1.isupper())
+print('字符串是不是以hello开头：', str1.startswith('hello'))
+print('字符串是不是以hello结尾：', str1.endswith('hello'))
+print('字符串是不是以!开头：', str1.startswith('!'))
+print('字符串是不是以!结尾：', str1.endswith('!'))
+
+
